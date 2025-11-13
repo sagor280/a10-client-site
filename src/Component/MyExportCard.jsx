38432f6bed409a3940cd8eb762f7f40743ec3505
sagor28 <bdsagorkumar8@gmail.com>
2025-11-13 11:32:15 +0600

    delete product UI & database

--- src/Component/MyExportCard.jsx
+++ src/Component/MyExportCard.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import { FaPencilAlt, FaTrash } from 'react-icons/fa';
-import { Link } from 'react-router'; 
+import { Link, useNavigate } from 'react-router'; 
+import Swal from 'sweetalert2';
 
 const MyExportCard = ({ product }) => {
+  const navigate = useNavigate();
   const { _id, name, imageUrl, price, origin, rating, quantity } = product;
+
+
+  //  Delete handler 
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/products/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.success) {
+              // UI theke o remove korbo
+          navigate('/all-products')
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your import has been removed successfully.",
+                icon: "success",
+              });
+            } else {
+              Swal.fire("Error!", "Failed to delete this item.", "error");
+            }
+          })
+          .catch((err) => {
+            console.error("Delete error:", err);
+            Swal.fire("Error!", "Something went wrong.", "error");
+          });
+      }
+    });
+  };
+
 
   return (
     <div className="bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition-transform duration-300 hover:-translate-y-1 border border-gray-100 max-w-sm">
@@ -34,6 +75,7 @@ const MyExportCard = ({ product }) => {
             <FaPencilAlt /> Update
           </Link>
           <button
+          onClick={() => handleDelete(_id)}
             className="flex items-center justify-center gap-2 bg-red-500 text-white px-4 py-2 rounded-full font-medium hover:bg-red-600 transition duration-300 flex-1"
           >
             <FaTrash /> Delete
